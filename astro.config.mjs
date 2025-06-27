19dd1fa047fccbedb28bdc30670404a60e65819f
unibox <unibox@duck.com>
2025-06-27 15:36:51 +0200

    add base.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-nexter.netlify.app',
   integrations: [sitemap()],
-  adapter: netlify()
+  adapter: netlify(),
 });
