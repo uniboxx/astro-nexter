f3add41fa072704c074854e7e72a4eb629d26fc2
unibox <unibox@duck.com>
2025-06-27 15:13:24 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap()],
+  adapter: netlify()
+});
